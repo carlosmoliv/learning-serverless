aa8216fb103cd56be64dc02c79770461da02b3e8
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-08-12 11:26:57 -0300

    secure deploy with aws ssm

--- src/handler.js
+++ src/handler.js
@@ -1,11 +1,25 @@
 const serverless = require("serverless-http");
 const express = require("express");
 const { neon, neonConfig } = require("@neondatabase/serverless");
+const AWS = require("aws-sdk");
+
+const AWS_REGION = "us-east-1";
+
 const app = express();
+const ssm = new AWS.SSM({ region: AWS_REGION });
+
+const DATABASE_URL_SSM_PARAM = "/serverless-nodejs/prod/database-url";
 
 async function dbClient() {
+  const paramStoreData = await ssm
+    .getParameter({
+      Name: DATABASE_URL_SSM_PARAM,
+      WithDecryption: true,
+    })
+    .promise();
+
   neonConfig.fetchConnectionCache = true;
-  const sql = neon(process.env.DATABASE_URL);
+  const sql = neon(paramStoreData.Parameter.Value);
   return sql;
 }
 
