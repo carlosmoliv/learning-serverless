3f2498b780e2ccbd0701a4078c5a04189e29be1c
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-08-12 00:42:27 -0300

    add first deploy setup

---
+++ src/handler.js
@@ -0,0 +1,34 @@
+const serverless = require("serverless-http");
+const express = require("express");
+const { neon, neonConfig } = require("@neondatabase/serverless");
+const app = express();
+
+async function dbClient() {
+  neonConfig.fetchConnectionCache = true;
+  const sql = neon(process.env.DATABASE_URL);
+  return sql;
+}
+
+app.get("/", async (req, res, next) => {
+  const sql = await dbClient();
+  const results = await sql`select now();`;
+
+  return res.status(200).json({
+    message: "Hello from root!",
+    results,
+  });
+});
+
+app.get("/hello", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from path!",
+  });
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+exports.handler = serverless(app);
