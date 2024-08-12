e1ea3f3018cbf1841a960ff031ecbb7177903b12
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-08-12 12:17:19 -0300

    decouple and upgrade aws ssm client

--- src/handler.js
+++ src/handler.js
@@ -1,35 +1,18 @@
-const serverless = require("serverless-http");
 const express = require("express");
-const { neon, neonConfig } = require("@neondatabase/serverless");
-const AWS = require("aws-sdk");
-
-const AWS_REGION = "us-east-1";
+const serverless = require("serverless-http");
+const { getDbClient } = require("./db/clients");
 
 const app = express();
-const ssm = new AWS.SSM({ region: AWS_REGION });
-
-const DATABASE_URL_SSM_PARAM = "/serverless-nodejs/prod/database-url";
-
-async function dbClient() {
-  const paramStoreData = await ssm
-    .getParameter({
-      Name: DATABASE_URL_SSM_PARAM,
-      WithDecryption: true,
-    })
-    .promise();
-
-  neonConfig.fetchConnectionCache = true;
-  const sql = neon(paramStoreData.Parameter.Value);
-  return sql;
-}
 
 app.get("/", async (req, res, next) => {
-  const sql = await dbClient();
-  const results = await sql`select now();`;
+  const sql = await getDbClient();
+  const now = Date.now();
+  const [dbNowResult] = await sql`select now();`;
+  const delta = (dbNowResult.now.getTime() - now) / 1000;
 
   return res.status(200).json({
     message: "Hello from root!",
-    results,
+    delta,
   });
 });
 
